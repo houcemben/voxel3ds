2f67b903128d61cea800c75653d7ad9d0e2b895d
houcemben <57008747+houcemben@users.noreply.github.com>
2024-12-07 15:24:25 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -18,21 +18,21 @@
         </div>
 
         <!-- Texte sous le logo -->
-        <p>..site under construction..<br>
+        <p>..site under construction..</p>
 
         <!-- Section des icônes sociales -->
         <div class="social-icons">
-            <a href="https://www.facebook.com/Voxel3DStudio" target="_blank">
-                <img src="icons/facebook-icon.png" alt="Facebook">
+            <a href="https://www.facebook.com/Voxel3DStudio" target="_blank" aria-label="Facebook">
+                <img src="icons/facebook-icon.png" alt="Facebook Icon">
             </a>
-            <a href="https://tn.linkedin.com/company/voxel-3d-studio" target="_blank">
-                <img src="icons/linkedin-icon.png" alt="LinkedIn">
+            <a href="https://tn.linkedin.com/company/voxel-3d-studio" target="_blank" aria-label="LinkedIn">
+                <img src="icons/linkedin-icon.png" alt="LinkedIn Icon">
             </a>
-            <a href="https://www.instagram.com/voxel3dstudio/" target="_blank">
-                <img src="icons/instagram-icon.png" alt="Instagram">
+            <a href="https://www.instagram.com/voxel3dstudio/" target="_blank" aria-label="Instagram">
+                <img src="icons/instagram-icon.png" alt="Instagram Icon">
             </a>
-            <a href="https://maps.app.goo.gl/upsNNdjPBKqfhgd67" target="_blank">
-                <img src="icons/maps-icon.png" alt="Maps">
+            <a href="https://maps.app.goo.gl/upsNNdjPBKqfhgd67" target="_blank" aria-label="Google Maps">
+                <img src="icons/maps-icon.png" alt="Google Maps Icon">
             </a>
         </div>
     </div>
